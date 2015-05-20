08e8c380a20ac600f68989273d73c20a0a074be5
Joey Lawrance <lawrancej@wit.edu>
2015-05-20 13:11:22 -0400

    Remotes.

---
+++ remotes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git remote add Hoffmanm2-github	git@github.com:Hoffmanm2/illacceptanything.git 
+git remote add LeoChaisson-github	git@github.com:LeoChaisson/illacceptanything.git 
+git remote add Lunisce-github	git@github.com:Lunisce/illacceptanything.git 
+git remote add MediumSoda-github	git@github.com:MediumSoda/illacceptanything.git 
+git remote add MusicAdam-github	git@github.com:MusicAdam/illacceptanything.git 
+git remote add OzzyOrc-github	git@github.com:OzzyOrc/illacceptanything.git 
+git remote add acs930-github	git@github.com:acs930/illacceptanything.git 
+git remote add almutairit-github	git@github.com:almutairit/illacceptanything.git 
+git remote add bigmacmccoy-github	git@github.com:bigmacmccoy/illacceptanything.git 
+git remote add chaskaransingh-github	git@github.com:chaskaransingh/illacceptanything.git 
+git remote add einsidler-github	git@github.com:einsidler/illacceptanything.git 
+git remote add jigoolik-github	git@github.com:jigoolik/illacceptanything.git 
+git remote add joey-walker-github	git@github.com:joey-walker/illacceptanything.git 
+git remote add kpombo-github	git@github.com:kpombo/illacceptanything.git 
+git remote add lawrancej-github	git@github.com:lawrancej/illacceptanything.git 
+git remote add miahjees-github	git@github.com:miahjees/illacceptanything.git 
+git remote add mmichaud93-github	git@github.com:mmichaud93/illacceptanything.git 
+git remote add pereiraj1-github	git@github.com:pereiraj1/illacceptanything.git 
+git remote add sarthakcfc-github	git@github.com:sarthakcfc/illacceptanything.git 
+git remote add tennentb-github	git@github.com:tennentb/illacceptanything.git 
+git remote add tule93-github	git@github.com:tule93/illacceptanything.git 
+git remote add whippenb-github	git@github.com:whippenb/illacceptanything.git 
+git remote add williamsg3-github	git@github.com:williamsg3/illacceptanything.git 
